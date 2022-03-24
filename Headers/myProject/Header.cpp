5a1cfac5042f7e5cf3afed27b36e6742002e002e
HiLiber <henny90974@gamil.com>
2022-03-24 18:34:02 +0200

    commit the header

---
+++ Headers/myProject/Header.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+#include <iostream>
+
+void writeNumber(int num)
+{
+    cout << "the number:" << num;
+}
+
+int readNumber()
+{
+    int x;
+    cout << "enter number:";
+    cin >> x;
+    return x;
+}
---
+++ Headers/myProject/myProject.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+//int readNumber() {
+//    int x;
+//    cout << "enter number:";
+//    cin >> x;
+//    return x;
+//}
+//
+//void writeNumber(int num) {
+//    cout << "the number:" << num;
+//}
+
+int main()
+{
+    int x = readNumber();
+    int y = readNumber();
+    writeNumber(x + y);
+}
+
